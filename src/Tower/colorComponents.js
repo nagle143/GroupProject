4f4128ad826492202469a023473818974c89cf4d
nagle143 <naglet@ksu.edu>
2017-11-29 23:27:44 -0600

    Added Documentation/Comments

--- src/Tower/colorComponents.js
+++ src/Tower/colorComponents.js
@@ -1,20 +1,398 @@
 
-
+/** @class Color
+* Class determines the color components that feed into the towers
+*/
 export default class Color {
+  /** @constructor
+  * initialzes all the variables important with color components
+  * @param {float} x - x position
+  * @param {float} y - y position
+  * @param {string} color - name of the color,  should be RGB but will work with RGBMYC & White
+  */
   constructor(x, y, color) {
     this.x = x;
     this.y = y;
-    this.color = color;
+    this.color = null;
+    //variables to help keep track of component dominance
+    this.components = {first: null, second: null};
+    //Keeps track of how dominant the first component is
+    this.dominance = 0.0;
+    //Overall level of the gem
+    this.level = 1;
     //Level one Starters
-    this.minDamage = 5;
-    this.MaxDamage = 10;
+    this.minDamage = 0;
+    this.maxDamage = 0;
+    this.initDamage(color);
+    //I beleive 60 = 1 second
+    this.rateOfFire = 60;
+    this.range = 100;
+    this.radius = 25;
+  }
+
+  /** @function initDamageColor
+    * Function to assign starting damage values, in case we want to change the values of the the different colors
+    * Also assigns RGB values to the color property
+    * @param {string} color - the color the component is suppose to be
+    */
+  initDamage(color) {
+    switch (color) {
+      case 'red':
+        this.minDamage = 5;
+        this.maxDamage = 10;
+        this.color = 'red';
+        this.components.first = 'red';
+        this.dominance = 1.00;
+        break;
+      case 'blue':
+        this.minDamage = 5;
+        this.maxDamage = 10;
+        this.color = 'blue';
+        this.components.first = 'blue';
+        this.dominance = 1.00;
+        break;
+      case 'green':
+        this.minDamage = 5;
+        this.maxDamage = 10;
+        this.color = 'green';
+        this.components.first = 'green';
+        this.dominance = 1.00;
+        break;
+      case 'cyan':
+        this.minDamage = 5;
+        this.maxDamage = 10;
+        this.color = 'cyan';
+        this.components.first = 'blue';
+        this.components.second = 'green';
+        this.dominance = 0.50;
+        break;
+      case 'magenta':
+        this.minDamage = 5;
+        this.maxDamage = 10;
+        this.color = 'magenta';
+        this.components.first = 'red';
+        this.components.second = 'blue';
+        this.dominance = 0.50;
+        break;
+      case 'yellow':
+        this.minDamage = 5;
+        this.maxDamage = 10;
+        this.color = 'yellow';
+        this.components.first = 'red';
+        this.components.second = 'green';
+        this.dominance = 0.50;
+        break;
+      case 'white':
+        this.minDamage = 5;
+        this.maxDamage = 10;
+        this.color = 'white';
+        this.components.first = 'white';
+        this.dominance = 1.00;
+        break;
+    }
+  }
+
+  /** @function determineSpecials
+  * Function to determine the special properties of the projectiles
+  * @returns {special} effect & time - effect can be a float, int or null
+  * time can be and int or null. Both are null if color is White
+  */
+  determineSpecials() {
+    let base = null;
+    let time = null;
+    switch (this.color) {
+      case 'red':
+        base = Math.round(this.minDamage * 0.30);
+        time = 60;
+        time += this.level * 20;
+        break;
+      case 'green':
+        base = 0.01;
+        time = 120;
+        base *= this.level;
+        time += this.level * 30;
+        break;
+      case 'blue':
+        base = 0.05;
+        base *= this.level;
+        break;
+      case 'magenta':
+        time = 30;
+        time += this.level * 5;
+        break;
+      case 'yellow':
+        time = 10;
+        time += Math.round(this.level * 0.5 * time);
+        break;
+      case 'cyan':
+        base = 0.05;
+        base *= this.level * 0.5;
+        time = 120;
+        time += this.level * 15;
+        break;
+      default:
+        return null;
+    }
+    return {effect: base, timer: time};
+  }
+
+  /** @function combine
+  * Function to handle combining the stats of the color components and colors
+  * @param {color} component - an actual color component
+  * IMPORANT - function assumes both components are the same level
+  */
+  combine(component) {
+    //Damage is added together with a slight bump
+    this.minDamage = (component.minDamage + this.minDamage) * 1.05;
+    this.maxDamage = (component.maxDamage + this.maxDamage) * 1.05;
+    //Reduce the rate of fire by 5%
+    this.rateOfFire = Math.floor(this.rateOfFire *  0.95);
+    //Increase the range by 10%
+    this.range = Math.floor(this.range * 1.10);
+    this.level++;
+    //If the color of the components are the same or the component being add is white, we are done
+    if(this.color === component.color || component.color === 'white') {
+      return;
+    }
+    //If this component is white, it takes on the color of the second component
+    if(this.color === 'white') {
+      this.color = component.color;
+      this.component.first = component.component.first;
+      this.component.second = component.component.second;
+      this.dominance = component.dominance;
+      return;
+    }
+    /*
+    Notes - primary colors RGB mix to create mixed colors MYC,
+    Mixed colors combine to make the primary color they have in common
+    Complementary colors mix to create white (RGB system is additive)
+    Nested switch statements are the only way I could see doing this, and it opperates quickly, and won't excute often, so it should be alright, but it is ugly.
+    */
+    switch (this.color) {
+      case 'red':
+        switch (component.color) {
+          case 'green':
+            this.color = 'yellow';
+            this.components.second = 'yellow';
+            this.dominance = 0.5;
+            break;
+          case 'blue':
+            this.color = 'magenta';
+            this.components.second = 'blue';
+            this.dominance = 0.5;
+            break;
+          case 'magenta':
+            this.color = 'magenta';
+            this.components.second = 'blue';
+            this.dominance = +((this.dominance + component.dominance) / 2).toFixed(2);;
+            break;
+          case 'yellow':
+            this.color = 'yellow';
+            this.components.second = 'green';
+            this.dominance = +((this.dominance + component.dominance) / 2).toFixed(2);;
+            break;
+          case 'cyan':
+            this.color = 'white';
+            this.components.first = 'white';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+        }
+        break;
+      case 'green':
+        switch (component.color) {
+          case 'red':
+            this.color = 'yellow';
+            this.components.first = 'red';
+            this.components.second = 'green';
+            this.dominance = 0.5;
+            break;
+          case 'blue':
+            this.color = 'cyan';
+            this.components.first = 'blue';
+            this.components.second = 'green';
+            this.dominance = 0.5;
+            break;
+          case 'magenta':
+            this.color = 'white';
+            this.components.first = 'white';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+          case 'yellow':
+            this.color = 'yellow';
+            this.components.first = 'red';
+            this.components.second = 'green';
+            this.dominance = +(component.dominance / 2).toFixed(2);;
+            break;
+          case 'cyan':
+            this.color = 'cyan';
+            this.components.first = 'blue';
+            this.components.second = 'green';
+            this.dominance = +(component.dominance / 2).toFixed(2);;
+            break;
+        }
+        break;
+      case 'blue':
+        switch (component.color) {
+          case 'red':
+            this.color = 'magenta';
+            this.components.first = 'red';
+            this.components.second = 'blue';
+            this.dominance = 0.5;
+            break;
+          case 'green':
+            this.color = 'cyan';
+            this.components.first = 'blue';
+            this.components.second = 'green';
+            this.dominance = 0.5;
+            break;
+          case 'magenta':
+            this.color = 'magenta';
+            this.components.first = 'red';
+            this.components.second = 'blue';
+            this.dominance = +(component.dominance / 2).toFixed(2);;
+            break;
+          case 'yellow':
+            this.color = 'white';
+            this.components.first = 'white';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+          case 'cyan':
+            this.color = 'cyan';
+            this.components.first = 'blue';
+            this.components.second = 'green';
+            this.dominance = +((this.dominance + component.dominance) / 2).toFixed(2);;
+            break;
+        }
+        break;
+      case 'magenta':
+        switch (component.color) {
+          case 'red':
+            this.color = 'magenta';
+            this.components.first = 'red';
+            this.components.second = 'blue';
+            this.dominance = +((this.dominance + component.dominance) / 2).toFixed(2);;
+            break;
+          case 'green':
+            this.color = 'white';
+            this.components.first = 'white';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+          case 'blue':
+            this.color = 'magenta';
+            this.components.first = 'red';
+            this.components.second = 'blue';
+            this.dominance = +(this.dominance / 2).toFixed(2);;
+            break;
+          case 'yellow':
+            this.color = 'red';
+            this.components.first = 'red';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+          case 'cyan':
+            this.color = 'blue';
+            this.components.first = 'blue';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+        }
+        break;
+      case 'yellow':
+        switch (component.color) {
+          case 'red':
+            this.color = 'yellow';
+            this.components.first = 'red';
+            this.components.second = 'green';
+            this.dominance = +((this.dominance + component.dominance) / 2).toFixed(2);;
+            break;
+          case 'green':
+            this.color = 'yellow';
+            this.components.first = 'red';
+            this.components.second = 'green';
+            this.dominance = +(this.dominance / 2).toFixed(2);;
+            break;
+          case 'blue':
+            this.color = 'white';
+            this.components.first = 'white';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+          case 'magenta':
+            this.color = 'red';
+            this.components.first = 'red';
+            this.components.second = null;
+            this.dominance = 1.00;
+            break;
+          case 'cyan':
+            this.color = 'blue';
+            this.components.first = 'blue';
+            this.components.second = null;
+            this.dominance = 1.00;
+            break;
+        }
+        break;
+      case 'cyan':
+        switch (component.color) {
+          case 'red':
+            this.color = 'white';
+            this.components.first = 'white';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+          case 'green':
+            this.color = 'cyan';
+            this.components.first = 'blue';
+            this.components.second = 'green';
+            this.dominance = +(this.dominance / 2).toFixed(2);;
+            break;
+          case 'blue':
+            this.color = 'cyan';
+            this.components.first = 'blue';
+            this.components.second = 'green';
+            this.dominance = +((this.dominance + component.dominance) / 2).toFixed(2);;
+            break;
+          case 'magenta':
+            this.color = 'blue';
+            this.components.first = 'blue';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+          case 'yellow':
+            this.color = 'green';
+            this.components.first = 'green';
+            this.components.second = null;
+            this.dominance = 1.0;
+            break;
+        }
+        break;
+    }
+  }
+
+  /** @function energyMultipler
+  * function to account for the energy pool expanding, might move this to the tower
+  */
+  energyMultipler(multipler) {
+    this.minDamage *= multipler;
+    this.maxDamage *= multipler;
   }
 
   update() {
-
+    //? Don't know if I need this
   }
 
-  render() {
-
+  render(ctx) {
+    ctx.save();
+    ctx.strokeStyle = this.color;
+    ctx.beginPath();
+    ctx.translate(this.x, this.y);
+    ctx.moveTo(0, -this.radius);
+    ctx.lineTo(this.radius, 0);
+    ctx.lineTo(0, this.radius);
+    ctx.lineTo(-this.radius, 0);
+    ctx.closePath();
+    ctx.restore();
   }
 }
--- src/Tower/tower.js
+++ src/Tower/tower.js
@@ -1,19 +1,275 @@
 import Color from './colorComponents.js';
-//Import all structural components subclasses
-import Rapids from './Projectiles/rapid.js';
-//Import all projectile subclasses
-import RapidFire from './StructuralComponents/rapidFire.js';
 
+//Import all projectile components subclasses
+import Plasma from './Projectiles/plasma.js';
+import Grenade from './Projectiles/grenade.js';
+import Rail from './Projectiles/rail.js';
+import Light from './Projectiles/light.js';
+import AOE from './Projectiles/aoe.js';
+import Beam from './Projectiles/beam.js';
+import ChainShot from './Projectiles/chainshot.js';
+
+//Import all structural subclasses
+import PlasmaGun from './Structures/plasmagun.js';
+import Launcher from './Structures/launcher.js';
+import RailGun from './Structures/railgun.js';
+import LightGun from './Structures/lightgun.js';
+import Pulse from './Structures/pulse.js';
+import Multishot from './Structures/multishot.js';
+import Chain from './Structures/chain.js';
+
+/** @class Tower
+* Object that represents the tower as a whole, controls color and structural components and projectile creation
+*/
 export default class Tower {
-  constructor() {
-
+  /** @constructor
+  * Handles the creation of the tower's base properties
+  * @param {float} x - x position of the tower
+  * @param {float} y - y position of the tower, x & y should be within a Building
+  * @param {string} struct - the name of the struct to be initialized, can be null
+  * @param {string} color - the name of the color to be initialized, can be null
+  * Notes - struct or color can be null, but not both. Color should R,G, or Blue but can support any color
+  */
+  constructor(x, y, struct, color) {
+    this.x = x;
+    this.y = y;
+    this.color = null;
+    this.structural = null;
+    //Array of projectiles, type depends of the structural component
+    this.projectiles = [];
+    //Rate of fire variable, will be calculated later
+    this.rate = 0.0;
+    //Only initialzes the ones that are not null
+    if(color) {
+      this.addColor(color);
+    }
+    if(struct) {
+      this.addStruct(struct);
+    }
+    //Determine if the tower should be reloading
+    this.reloading = false;
+    //direction the tower is aiming, doesn't matter for some structural types
+    this.direction = 0.0;
+    //Array of targets, should only be 1 target unless structural is Multishot type
+    this.targets = [];
+    //Basic Tower stats to be calculated later
+    this.minDamage = 0.0;
+    this.maxDamage = 0.0;
+    this.range = 0.0;
   }
 
+  /** @function track
+  * Function to handle calculating the direction the tower will shoot
+  * Multishot & Pulse towers don't need this as direction is irrelavent
+  */
+  track() {
+    var dx = this.x - this.targets[0].x;
+    var dy = this.y - this.targets[0].y;
+    //Draw a line to the target
+    var distance = Math.sqrt(dx * dx + dy * dy);
+    //Get the direction to the target
+    var direction = Math.acos((dy)/ distance);
+    //Mirror the angle for the left hand side
+    if(dx > 0) {
+      direction *= -1;
+    }
+    this.direction = direction;
+  }
+
+  /** @function upgrade
+  * Very simple function to upgrade the color component of a tower
+  */
+  upgrade() {
+    this.color.combine(this.color);
+  }
+
+  /** @function addColor
+  * Function to initialze the color component of the tower
+  * @param {string} color - name of the color to be created
+  */
+  addColor(color) {
+    this.color = new Color(this.x, this.y, color);
+    this.rate = this.color.rateOfFire;
+  }
+
+  /** @function changeColor
+  * Function to handle swapping the color components
+  * @param {color} component - is an actuall color object
+  */
+  changeColor(component) {
+    this.color = component;
+  }
+
+  /** @function changeStruct
+  * Function to handle swapping the structural components
+  * @param {struct} struct - is an actual structural object
+  */
+  changeStruct(struct) {
+    this.struct = struct;
+  }
+
+  /** @function addStruct
+  * Function to add a structural component
+  * @param {string} struct - name of the struct to create
+  */
+  addStruct(struct) {
+    switch (struct) {
+      case "PlasmaGun":
+        this.structural = new PlasmaGun(this.x, this.y);
+        break;
+      case "Launcher":
+        this.structural = new Launcher(this.x, this.y);
+        break;
+      case "RailGun":
+        this.structural = new RailGun(this.x, this.y);
+        break;
+      case "LightGun":
+        this.structural = new LightGun(this.x, this.y);
+        break;
+      case "Pulse":
+        this.structural = new Pulse(this.x, this.y);
+        break;
+      case "Multishot":
+        this.structural = new Multishot(this.x, this.y);
+        break;
+      case "Chain":
+        this.structural = new Chain(this.x, this.y);
+        break;
+      default:
+        console.log("ERROR IN STRUCT TYPE");
+    }
+  }
+
+  /** @function createProjectile
+  * Function to create the appropriate projectile based on the color and struct components
+  */
+  createProjectile() {
+    //Calculate the damage value
+    let damage = Math.randomInt(this.minDamage, this.maxDamage + 1);
+    //Calculate how strong the special property of the projectile is
+    let special = this.color.determineSpecials();
+    //Set up x & y values for the projectiles, only some will need to be different than this.x & this.y
+    let x = 0;
+    let y = 0;
+    switch (this.structural.name) {
+      case "PlasmaGun":
+        x = this.x + Math.sin(this.direction) * 30;
+        y = this.y - Math.cos(this.direction) * 30;
+        this.projectiles.push(new Plasma(x, y, damage, this.direction, this.range, this.color.color, this.targets[0]));
+        break;
+      case "Launcher":
+        x = this.x + Math.sin(this.direction)* 30;
+        y = this.y - Math.cos(this.direction)* 30;
+        this.projectiles.push(new Grenade(x, y, damage, this.direction, this.range, this.color.color, this.targets[0]));
+        break;
+      case "RailGun":
+        x = this.x + Math.sin(this.direction)* 15;
+        y = this.y - Math.cos(this.direction)* 15;
+        this.projectiles.push(new Rail(x, y, damage, this.direction, this.range, this.color.color, this.targets[0]));
+        break;
+      case "LightGun":
+        x = this.x + Math.sin(this.direction)* 30;
+        y = this.y - Math.cos(this.direction)* 30;
+        this.projectiles.push(new Light(x, y, damage, this.direction, this.range, this.color.color, this.targets[0]));
+        break;
+      case "Pulse":
+        this.projectiles.push(new AOE(this.x, this.y, damage, this.direction, this.range, this.color.color, this.targets[0]));
+        break;
+      case "Multishot":
+        //Iterate over the targets to check if the monster is already being targeted
+        this.targets.forEach(target => {
+          let check = false;
+          for(let i = 0; i < this.projectiles.length; i++) {
+            if(this.projectiles[i].target === target) {
+              //If it is already being targeted we don't need to create a new projectile
+              check = true;
+            }
+          }
+          if(!check) {
+            //If the target is not being targeted current, create a new projectile
+            this.projectiles.push(new Beam(this.x, this.y, damage, this.direction, this.range, this.color.color, target));
+          }
+        });
+        break;
+      case "Chain":
+        x = this.x + Math.sin(this.direction)* 30;
+        y = this.y - Math.cos(this.direction)* 30;
+        this.projectiles.push(new ChainShot(x, y, damage, this.direction, this.range, this.color.color, this.targets[0]));
+        break;
+      default:
+        console.log("ERROR IN PROJECTILE CREATION");
+    }
+  }
+
+  /** @function structModifier
+  * Function to handle how the struct modifies the towers stats, white colored components get bonuses
+  */
+  structModifier() {
+    if(this.color.color === 'white') {
+      this.range = Math.round(1.10 * this.color.range * this.structural.rangeMOD);
+      this.minDamage = Math.round( 1.20 * this.color.minDamage * this.structural.damageMOD);
+      this.maxDamage = Math.round(1.20 * this.color.maxDamage * this.structural.damageMOD);
+    }
+    else {
+      this.range = Math.round(this.color.range * this.structural.rangeMOD);
+      this.minDamage = Math.round(this.color.minDamage * this.structural.damageMOD);
+      this.maxDamage = Math.round(this.color.maxDamage * this.structural.damageMOD);
+    }
+    this.rateOfFire = Math.round(this.color.rateOfFire * this.structural.rateOfFireMOD);
+  }
+
+  /** @function update
+  * Function that handles all updating to do with the tower and sub objects (projectiles and components)
+  */
   update() {
-
+    //Things to do if you have both types of components
+    if(this.color && this.structural) {
+      //Update tower stats
+      this.structModifier();
+      //If it has targets, track them
+      if(this.targets.length > 0) {
+        this.track();
+        this.structural.update(this.direction);
+      }
+      //If it is reloading update counter
+      if(this.reloading) {
+        this.rate--;
+        if(this.rate <= 0) {
+          this.rate = Math.round(this.color.rateOfFire * this.structural.rateOfFireMOD);
+          this.reloading = false;
+        }
+      }
+      //If it is not reloading & has targets, shoot
+      if(!this.reloading && this.targets.length > 0) {
+        this.createProjectile();
+        this.reloading = true;
+      }
+    }
+    //Update projectiles
+    for(let i = 0; i < this.projectiles.length; i++) {
+      if(this.projectiles[i].update()) {
+        this.projectiles.splice(i, 1);
+      }
+    }
+    //Will need to update struct too for spinning
   }
 
-  render() {
-
+  /** @function render
+  * Standar Render Function
+  * @param {context} ctx - back buffer context from game.js
+  */
+  render(ctx) {
+    ctx.save();
+    ctx.strokeStyle = 'white';
+    ctx.strokeRect(this.x - 15, this.y - 15, 30, 30);
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.range, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+    //Render projectiles
+    this.projectiles.forEach(projectile => {
+      projectile.render(ctx);
+    });
   }
 }
