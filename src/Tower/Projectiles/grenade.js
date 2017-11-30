b7f8edd11f904ae6be24984fd1c97c2a0cf044da
nagle143 <naglet@ksu.edu>
2017-11-29 23:29:05 -0600

    Working Projectiles

---
+++ src/Tower/Projectiles/grenade.js
@@ -0,0 +1,29 @@
+import Projectile from './projectile.js';
+
+export default class Grenade extends Projectile {
+  constructor(x, y, damage, direction, range, color, target, special, dominance) {
+    super(x, y, damage, direction, range, color, target, special, dominance);
+    this.life = 100;
+    this.AOE = 30;
+    this.mag = 4;
+    super.initSpeed();
+  }
+
+  update() {
+    if(Math.abs(this.x - this.target.x) < this.radius && Math.abs(this.y - this.target.y) < this.radius) {
+      this.radius = this.AOE;
+      this.life--;
+      if(this.life <= 0) {
+        return true;
+      }
+    }
+    else {
+      super.update();
+    }
+    return false;
+  }
+
+  render(ctx) {
+    super.render(ctx);
+  }
+}
--- src/Tower/Projectiles/projectile.js
+++ src/Tower/Projectiles/projectile.js
@@ -1,15 +1,42 @@
 //Projectile super class
 
 export default class Projectile {
-  constructor() {
+  constructor(x, y, damage, direction, range, color, target, special, dominance) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+    this.damage = damage;
+    this.range = range;
+    this.color = color;
+    this.dominance = dominance;
+    this.special = special;
+    this.radius = 10;
+    this.mag = 5;
+    this.speed = {x: 0.0, y: 0.0};
+    this.target = {x: target.x, y: target.y};
+  }
 
+  initSpeed() {
+    this.speed.x = Math.sin(this.direction) * this.mag;
+    this.speed.y = -Math.cos(this.direction) * this.mag;
   }
 
   update() {
-
+    this.x += this.speed.x;
+    this.y += this.speed.y;
   }
 
-  render() {
-
+  render(ctx) {
+    ctx.save();
+    ctx.strokeStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.target.x, this.target.y);
+    ctx.stroke();
+    ctx.restore();
   }
 }
