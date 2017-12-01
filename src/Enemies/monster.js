a01821e547256412c1606231103c19e849a2a32e
Dakota Lange <koty@ksu.edu>
2017-12-01 11:20:07 -0600

    added some more stuff to the super class needs reorginizing

--- src/Enemies/monster.js
+++ src/Enemies/monster.js
@@ -1,5 +1,5 @@
-//Monster super class
 
+//Monster super class
 export defualt class Monster
 {
     constructor(x, y,color,level)
@@ -36,10 +36,69 @@ export defualt class Monster
     }
 
 
-    status() // handles status effects from tower
+    status(color, time) // handles status effects from tower // this is wrong need to fix
+    {
+        if (this.CS == 0)
+        {
+            switch (color) {
+                case "red":
+                    burn(time);
+                    break;
+                case "cyan":
+                    slow(time);
+                    break;
+                case "yellow":
+                    stun(time);
+                    break;
+                case "green":
+                    shredArmor();
+                    break;
+                case "blue":
+                    energyGain();
+                    break;
+                case "magenta":
+                    Charm(time);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    // all status effects only apply if shields at 0
+
+    burn(time) // damage over time
     {
 
     }
+
+    slow(time) // slow based on percentage
+    {
+
+        this.currentSpeed = this.currentSpeed * .25; // slow by 25%
+    }
+
+    stun(time) // stun 
+    {
+
+    }
+
+    shredArmor() // destroy armor
+    {
+
+    }
+
+    energyGain() // gain more money based on a dot
+    {
+
+    }
+
+    Charm(time) // walk backwards in path
+    {
+
+    }
+
+    
 }
 
 
